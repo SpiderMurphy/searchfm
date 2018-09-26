1256093591580116307f751c5b5f414da73abf72
SpiderMurphy <vincenzocaleca@gmail.com>
2018-09-26 22:45:13 +0200

    Added presenter for search artist activity and edited the UI

--- app/src/main/java/com/radio/search_fm/MainActivity.java
+++ app/src/main/java/com/radio/search_fm/MainActivity.java
@@ -2,14 +2,42 @@ package com.radio.search_fm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
+import com.radio.search_fm.presenter.PresenterSearchArtist;
 import com.radio.search_fm.views.ViewSearchActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements ViewSearchActivity {
+    @BindView(R.id.searchToolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.artists)
+    RecyclerView mRwArtists;
+
+    // Presenter
+    private PresenterSearchArtist mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_artist, menu);
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/radio/search_fm/presenter/PresenterSearchArtist.java
@@ -0,0 +1,15 @@
+package com.radio.search_fm.presenter;
+
+import com.radio.search_fm.views.ViewSearchActivity;
+
+public class PresenterSearchArtist {
+    private ViewSearchActivity mView;
+
+    PresenterSearchArtist(ViewSearchActivity view) {
+        this.mView = view;
+    }
+
+    void searchArtistByName(String name){
+        
+    }
+}
